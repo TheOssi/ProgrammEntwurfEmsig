2615a2a71480f8ff68a86bf1a64a8e2bc73ee073
Kai Müller <kai.mueller01@sap.com>
2016-07-21 20:30:17 -0400

    shit merge

--- Diagramme & SQL/ERM in SQL.sql
+++ Diagramme & SQL/ERM in SQL.sql
@@ -1,4 +1,5 @@
 -- #### TODO #######
+-- länge bei strings
 -- mit AKD abgleich + Reihenfolge
 -- #################
 
@@ -14,31 +15,31 @@ USE HOCHZEITSPLANER;
 
 CREATE TABLE AktionsZustaende (
 	aktionsZustandID INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-	beschreibung TEXT NOT NULL,
+	beschreibung VARCHAR(250) NOT NULL,
 	PRIMARY KEY ( aktionsZustandID )
 );
 
 CREATE TABLE AktionsArten (
 	aktionsArtID INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-	beschreibung TEXT NOT NULL,
+	beschreibung VARCHAR(250) NOT NULL,
 	PRIMARY KEY ( aktionsArtID )
 );
 
 CREATE TABLE HilfsmittelArten (
 	hilfsmittelArtID INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-	beschreibung TEXT NOT NULL,
+	beschreibung VARCHAR(250) NOT NULL,
 	PRIMARY KEY ( hilfsmittelArtID )
 );
 
 CREATE TABLE Telefonnummern (
 	telefonnnummerID INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
-	telefonnnummer VARCHAR(15) NOT NULL,
+	telefonnnummer VARCHAR(100) NOT NULL,
 	PRIMARY KEY ( telefonnnummerID )
 );
 
 CREATE TABLE EmailAdressen (
 	emailAdresseID INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
-	emailAdresse VARCHAR(254) NOT NULL,
+	emailAdresse VARCHAR(100) NOT NULL,
 	PRIMARY KEY ( emailAdresseID )
 );
 
@@ -47,16 +48,16 @@ CREATE TABLE EmailAdressen (
 
 CREATE TABLE Personen (
  	personID INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
-	name TEXT NOT NULL,
-	adresse TEXT NOT NULL,
+	name VARCHAR(250) NOT NULL,
+	adresse VARCHAR(250) NOT NULL,
 	istDienstleister BOOL NOT NULL DEFAULT FALSE,		
 	PRIMARY KEY ( personID )		
 );
 
 CREATE TABLE Hochzeitsveranstaltungen (
 	hochzeitsID INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
-	titel TEXT NOT NULL,
-	motto VARCHAR(200),
+	titel VARCHAR(250) NOT NULL,
+	motto VARCHAR(250),
 	hochzeitsmanagerID INT UNSIGNED NOT NULL,
 	hochzeitspaarID INT UNSIGNED NOT NULL,
 	PRIMARY KEY ( hochzeitsID ),
@@ -70,7 +71,7 @@ CREATE TABLE Hochzeitsveranstaltungen (
 
 CREATE TABLE Caterer (
 	catererID INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
-	beschreibung TEXT,
+	beschreibung VARCHAR(255),
 	kontaktPerson INT UNSIGNED NOT NULL,
 	zumVergleich BOOL NOT NULL DEFAULT FALSE,
 	PRIMARY KEY ( catererID ),
@@ -81,10 +82,10 @@ CREATE TABLE Caterer (
 
 CREATE TABLE Aktionen (
 	aktionID INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
-	titel TEXT NOT NULL,
-	beschreibung TEXT NOT NULL,
+	titel VARCHAR(250) NOT NULL,
+	beschreibung VARCHAR(250) NOT NULL,
 	hochzeitsID INT UNSIGNED NOT NULL,
-	notiz TEXT,
+	notiz VARCHAR(250),
 	prioritaet TINYINT NOT NULL,
 	datum DATETIME NOT NULL,
 	dauer TIME NOT NULL,
@@ -106,7 +107,7 @@ CREATE TABLE Aktionen (
 
 CREATE TABLE Nahrungsmittel (
 	nahrungsmittelID INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
-	beschreibung TEXT,
+	beschreibung VARCHAR (250),
 	menge DOUBLE UNSIGNED NOT NULL,
 	mengenbeschreibung VARCHAR(50) NOT NULL,
 	PRIMARY KEY ( nahrungsmittelID )
@@ -114,8 +115,8 @@ CREATE TABLE Nahrungsmittel (
 
 CREATE TABLE Medien(
 	mediumID INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
-	uri TEXT NOT NULL UNIQUE,
-	title TEXT NOT NULL,
+	uri VARCHAR(250) NOT NULL UNIQUE,
+	title VARCHAR(250) NOT NULL,
 	PRIMARY KEY ( mediumID )
 );
 
@@ -123,16 +124,16 @@ CREATE TABLE Belege(
 	belegID INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
 	kosten DOUBLE UNSIGNED NOT NULL,
 	währung CHAR(3) NOT NULL,
-	title TEXT NOT NULL,
-	beschreibung TEXT, 
+	title VARCHAR(250) NOT NULL,
+	beschreibung VARCHAR(250), 
 	private BOOL NOT NULL,
 	PRIMARY KEY ( belegID )
 );
 
 CREATE TABLE Hilfsmittel (
 	hilfsmittelID INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
-	title TEXT NOT NULL,
-	beschreibung TEXT,
+	title VARCHAR(250) NOT NULL,
+	beschreibung VARCHAR(250),
 	hilfsmittelArt INT UNSIGNED NOT NULL,
 	PRIMARY KEY ( hilfsmittelID ),
 	FOREIGN KEY ( hilfsmittelArt ) REFERENCES HilfsmittelArten(hilfsmittelArtID)
@@ -142,12 +143,12 @@ CREATE TABLE Hilfsmittel (
 
 CREATE TABLE Orte (
 	ortID INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-	strasse VARCHAR(100) NOT NULL,
+	strasse VARCHAR(250) NOT NULL,
 	hausnummer VARCHAR(10) NOT NULL,
-	adressZusatz VARCHAR(100),
-	stadt VARCHAR(100),
-	postleitzahl VARCHAR(15) NOT NULL,
-	provinz VARCHAR(100) NOT NULL,
+	adressZusatz VARCHAR(250),
+	stadt VARCHAR(250),
+	postleitzahl VARCHAR(50) NOT NULL,
+	provinz VARCHAR(250) NOT NULL,
 	land CHAR(3) NOT NULL,
 	PRIMARY KEY ( ortID )
 );
